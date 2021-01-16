867b4164e8b7ea4fd874233ba0f981a076230b94
Paul J. Iutzi <pji@mac.com>
2021-01-16 12:46:13 -0600

    Initial implementation of connect.Token complete.

--- pjisocial/connect.py
+++ pjisocial/connect.py
@@ -4,9 +4,29 @@ connect
 
 Basic HTTP connectivity for the pjisocial module.
 """
+from keyring import get_password
+
+
+# Exceptions.
+class SecretDoesNotExist(RuntimeError):
+    """The OS's secret store doesn't contain the requested secret."""
+
+
 # Public classes.
 class Token:
     """A secret stored in the OS's native secret store."""
     def __init__(self, service: str, user: str) -> None:
         self.service = service
         self.user = user
+    
+    def __repr__(self):
+        return f"Token('{self.service}', '{self.user}')"
+
+    # Public methods.
+    def get(self) -> str:
+        """Return the value of the secret."""
+        secret = get_password(self.service, self.user)
+        if not secret:
+            msg = 'Expected secret not in OS secret store.'
+            raise SecretDoesNotExist(msg)
+        return secret
--- tests/test_connect.py
+++ tests/test_connect.py
@@ -5,7 +5,6 @@ test_connect
 Unit tests for the pjisocial.connect module.
 """
 import unittest as ut
-from unittest.mock import patch
 
 import keyring
 
@@ -16,10 +15,16 @@ from pjisocial import connect as cx
 class TokenTestCase(ut.TestCase):
     def setUp(self):
         """Create common test data and state."""
+        # Create the test secret.
         self.service = '_pjisocial_test_service'
         self.user = '_pjisocial'
         self.value = '_pjisocial_secret'
         keyring.set_password(self.service, self.user, self.value)
+        
+        # Ensure the non-existing secret doesn't exist.
+        self.not_service = '_pjisocial_not_service'
+        if keyring.get_password(self.not_service, self.user):
+            raise RunTimeError(f'Abort. {self.not_service} exists.')
 
     def test_initialize(self):
         """Given a service and a username, the Token class should
@@ -32,16 +37,65 @@ class TokenTestCase(ut.TestCase):
         }
 
         # Run test.
-        obj = cx.Token(**exp)
+        token = cx.Token(**exp)
 
         # Extract test result.
         act = {
-            'service': obj.service,
-            'user': obj.user,
+            'service': token.service,
+            'user': token.user,
         }
 
         # Determine if test passed.
         self.assertDictEqual(exp, act)
+    
+    def test_get(self):
+        """When called, the get() method should return the value of the
+        secret as a string.
+        """
+        # Expected values.
+        exp = self.value
+        
+        # Create specific test data and state.
+        token = cx.Token(self.service, self.user)
+        
+        # Run test.
+        act = token.get()
+        
+        # Determine if test passed.
+        self.assertEqual(exp, act)
+    
+    def test_get_secret_does_not_exist(self):
+        """If the secret doesn't exist in the OS's secret store, get()
+        should raise a connect.SecretDoesNotExist exception.
+        """
+        # Expected value.
+        exp = cx.SecretDoesNotExist
+        exp_msg = 'Expected secret not in OS secret store.'
+        
+        # Set up specific test data and state.
+        token = cx.Token(self.not_service, self.user)
+        
+        # Determine if test passed when block completes.
+        with self.assertRaisesRegex(exp, exp_msg):
+            
+            # Run test.
+            _ = token.get()
+
+    def test_repr(self):
+        """When needed, Token objects should return a representation of
+        themselves useful for debugging.
+        """
+        # Expected value.
+        exp = f"Token('{self.service}', '{self.user}')"
+        
+        # Set up specific test data and state.
+        token = cx.Token(self.service, self.user)
+        
+        # Run test.
+        act = repr(token)
+        
+        # Determine is test passed.
+        self.assertEqual(exp, act)
 
     def tearDown(self):
         keyring.delete_password(self.service, self.user)
