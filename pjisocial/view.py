acb670c4c83501a0cb3a99d5439428190be91d2b
Paul J. Iutzi <pji@mac.com>
2021-09-27 07:52:51 -0500

    pywebview can be redirected to external page.

---
+++ pjisocial/view.py
@@ -0,0 +1,15 @@
+import webview                                      # type: ignore
+
+
+def get_redirect_html(filename: str) -> str:
+    with open(filename) as fh:
+        doc = fh.read()
+    return doc
+
+
+filename = 'pjisocial/html/redirect.html'
+redirect = 'https://google.com'
+html = get_redirect_html(filename)
+redirect_html = html.format(redirect)
+webview.create_window('CNN Redirect', html=redirect_html)
+webview.start()
